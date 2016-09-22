bebed184557f7627c74c5e2368e1eb984152cd78
escamoteur <thomas@burkharts.net>
2016-09-22 13:24:35 +0200

    First commit

---
+++ IStreamingProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreamingRxCommons
+{
+    public interface IStreamingProvider
+    {
+        ObservableCollection<IAlbum> GetNewReleases(int limit, int offset = 0);
+    }
+}
